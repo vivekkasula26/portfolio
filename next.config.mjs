6a105e5e5e94038fe03baa1c5673317012bb0b3d
vivekkasula26 <vivekkasula26@gmail.com>
2025-01-29 15:14:01 -0500

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true, // GitHub Pages does not support Next.js Image Optimization
   },
-  basePath: "/vivekkasula26.github.io", // Replace with your GitHub repo name
-  assetPrefix: "/vivekkasula26.github.io/",
 };
 
 export default nextConfig;
