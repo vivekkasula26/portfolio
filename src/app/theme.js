58d01c2728145a12ead2e3d0e0df8b96777572a8
vivekkasula26 <vivekkasula26@gmail.com>
2025-01-29 21:41:52 -0500

    Mode default

--- src/app/theme.js
+++ src/app/theme.js
@@ -3,8 +3,14 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
+    mode: "light",
     primary: {
       main: "#181818", // Set the primary color to #181818
+    },
+  },
+  typography: {
+    body1: {
+      color: "black", // Custom color for body text
     },
   },
 });
